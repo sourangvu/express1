93e5dcdf5ac3e69e8633c238745f9e34dc7436fe
sourangvu <sourangvu@gmail.com>
2024-11-04 15:15:16 +0530

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 const express = require('express')
-const mongoose = require('mongoose');
 const app = express()
 const port = 3000
-
-async function main() {
-  await mongoose.connect('mongodb+srv://sourangvu:sourangvu@maincluster.idci6.mongodb.net/?retryWrites=true&w=majority&appName=Maincluster');
-
-}
-main()
-.then(res => {
-    console.log("DB connected")
-})
-.catch(err => console.log(err));
-
-
-const TaskSchema = new mongoose.Schema({
-  task: String,
-  iscompleted:String,
-  user:String
-});
-
-const Task = mongoose.model('task', TaskSchema);
 
 
 app.get('/', (req, res) => {
@@ -31,11 +11,11 @@ app.post('/', (req, res) => {
   res.send('Response for POST request')
 })
 
-app.put('/:id', (req, res) => {
+app.put('/', (req, res) => {
   res.send('Response for PUT request')
 })
 
-app.delete('/:id', (req, res) => {
+app.delete('/', (req, res) => {
   res.send('Response for DELETE request')
 })
 
